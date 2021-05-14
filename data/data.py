f018f7746600501d0b651c5fca2afe3887b280e4
Philipp Rindler <philipp.rindler@gmail.com>
2021-05-14 22:07:16 +0200

    Adult data added

--- data/data.py
+++ data/data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def load_wine_quality(type: str ='red') -> pd.DataFrame:
-    """Loads the wine quality data from the UCI Machine Learnind Database.
+    """Loads the wine quality data from the UCI Machine Learning Database.
     The data is described [here](https://archive.ics.uci.edu/ml/datasets/wine+quality)
     There are eleven features:
     1 - Fixed Acidity
@@ -29,10 +29,10 @@ def load_wine_quality(type: str ='red') -> pd.DataFrame:
         type (str, optional): Which data to return, must be 'red', 'white', or 'both'. Defaults to 'red'.
 
     Returns:
-        pd.DataFrame: Wine quality dataset
+        pd.DataFrame: If `type` is `'red'` or `'white'` a single DataFrame is returned. For `type='both'`, a tuple of DataFrames `(df_red, df_white)` is returned
     """
     
-    assert type in ['red', 'white', 'both']
+    assert type in ['red', 'white', 'both'], "type has to be either 'red', 'white', or 'both'"
 
     if type != 'white':
         try:
@@ -50,16 +50,12 @@ def load_wine_quality(type: str ='red') -> pd.DataFrame:
             df_white = pd.read_feather('wine_quality_white.feather')
         if type == 'white':
             return df_white
-    
-    df_red['Type'] = 'Red'
-    df_white['Type'] = 'White'
 
-    return pd.concat([df_red, df_white])
+    return df_red, df_white
 
 
 def _download_wine_data():
 
-    
     def download_df(type):
         assert type in ['red', 'white']
         url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-' + type + '.csv'
@@ -72,3 +68,72 @@ def _download_wine_data():
     df_red.to_feather('wine_quality_red.feather', compression='lz4', version=2)
     df_white = download_df('white')
     df_white.to_feather('wine_quality_white.feather', compression='lz4', version=2)
+
+
+def _download_adult_data():
+    def download_df(type):
+        url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.' + type
+        names = ['Age', 'Workclass', 'Final Weight', 'Education', 'Years of Education', 'Marital Status', 
+        'Occupation', 'Relationship', 'Race', 'Sex', 'Capital Gain', 'Capital Loss', 'Hours per Week', 
+        'Native Country', 'Income']
+        df = pd.read_csv(url, header=0, names=names)
+        return df
+    
+    df_train = download_df('data')
+    df_train.to_feather('adult_train.feather', compression='lz4', version=2)
+    df_test = download_df('test')
+    df_test.to_feather('adult_test.feather', compression='lz4', version=2)
+
+
+def load_adult_data(type: str='both') -> pd.DataFrame:
+    """Load adult data from the UCI Machine Learning Database.
+    The data is described [here](https://archive.ics.uci.edu/ml/datasets/adult)
+
+    There are 14 features
+
+    Age: continuous.
+    Workclass: Private, Self-emp-not-inc, Self-emp-inc, Federal-gov, Local-gov, State-gov, Without-pay, Never-worked.
+    Final Weight: continuous.
+    Education: Bachelors, Some-college, 11th, HS-grad, Prof-school, Assoc-acdm, Assoc-voc, 9th, 7th-8th, 12th, Masters, 1st-4th, 10th, Doctorate, 5th-6th, Preschool.
+    Years of Education: continuous.
+    Marital Status: Married-civ-spouse, Divorced, Never-married, Separated, Widowed, Married-spouse-absent, Married-AF-spouse.
+    Occupation: Tech-support, Craft-repair, Other-service, Sales, Exec-managerial, Prof-specialty, Handlers-cleaners, Machine-op-inspct, Adm-clerical, Farming-fishing, Transport-moving, Priv-house-serv, Protective-serv, Armed-Forces.
+    Relationship: Wife, Own-child, Husband, Not-in-family, Other-relative, Unmarried.
+    Race: White, Asian-Pac-Islander, Amer-Indian-Eskimo, Other, Black.
+    Sex: Female, Male.
+    Capital Gain: continuous.
+    Capital Loss: continuous.
+    Hours per Week: continuous.
+    Native Country: United-States, Cambodia, England, Puerto-Rico, Canada, Germany, Outlying-US(Guam-USVI-etc), India, Japan, Greece, South, China, Cuba, Iran, Honduras, Philippines, Italy, Poland, Jamaica, Vietnam, Mexico, Portugal, Ireland, France, Dominican-Republic, Laos, Ecuador, Taiwan, Haiti, Columbia, Hungary, Guatemala, Nicaragua, Scotland, Thailand, Yugoslavia, El-Salvador, Trinadad&Tobago, Peru, Hong, Holand-Netherlands.ge: continuous.
+
+    and the target variable:
+    Income: >= 50k, <50k
+
+    Args:
+        type (str, optional): Which data to load. Must be 'train', 'test', or 'both'. Defaults to 'both'.
+
+    Returns:
+        pd.DataFrame: If `type` is `'train'` or `'test'` a single DataFrame is returned, For `type='both'`, a tuple of DataFrames `(df_train, df_test)` is returned
+    """
+    
+    assert type in ['train', 'test', 'both'], "type has to be either 'train', 'test', or 'both'"
+    
+    if type != 'test':
+        try:
+            df_train = pd.read_feather('adult_train.feather')
+        except FileNotFoundError:
+            _download_adult_data()
+            df_train = pd.read_feather('adult_train.feather')
+        if type=='train':
+            return df_train
+    
+    if type != 'train':
+        try:
+            df_test = pd.read_feather('adult_test.feather')
+        except FileNotFoundError:
+            _download_adult_data()
+            df_test = pd.read_feather('adult_test.feather')
+        if type=='test':
+            return df_test
+    
+    return df_train, df_test
